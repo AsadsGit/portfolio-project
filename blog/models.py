c15fa9b4875e3d9aaf42cc25299d7872fed8c194
AL Asad Khan <asad22376@outlook.com>
2020-04-14 18:21:06 +0530

    blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,9 @@ class blogpost(models.Model):
     title = models.CharField(max_length=250)
     pub_date = models.DateTimeField()
     body = models.TextField (max_length=2000)
+    
+    def __str__(self):
+        return self.title 
 
 
 
